bc99ea8d45fcd904267c12a4319c52d3cb1abdf7
r0100 <60950667+r0100@users.noreply.github.com>
2020-07-03 16:13:14 -0300

    Update main.js
    Retirei os filtros desse arquivo.

--- nodejs/main.js
+++ nodejs/main.js
@@ -32,17 +32,6 @@ for(let i = 0; i<iq[0].length; i++)
     iq[1][i] -= 127.5;
 }
 
-//coeficientes calculados no matlab
-let audio_filter = [0.0261, 0.1402, 0.3337, 0.3337, 0.1402, 0.0261];
-//let audio_filter = [1, 0, 0];
-let fm_filter = [0.0005, -0.0017, 0.0032, -0.0052, 0.0079, -0.0115, 0.0162, -0.0223, 0.0302, -0.0406, 0.0548, -0.0752, 
-0.1077, -0.1699, 0.3502, 0, -0.3502, 0.1699, -0.1077, 0.0752, -0.0548, 0.0406, -0.0302, 0.0223,
--0.0162, 0.0115, -0.0079, 0.0052, -0.0032, 0.0017, -0.0005];
-
-//aprender como calcular esses
-//let lsb_filter = [];
-//let usb_filter = [];
-
 //let y = am.amiqdemod(iq, audio_filter);
 let y = fmiqdemod(iq, fm_filter, audio_filter);
 console.log(y);
