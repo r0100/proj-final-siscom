05652352a03ffc13b35c93b73a146e8d7ec96863
r0100 <60950667+r0100@users.noreply.github.com>
2020-07-03 16:16:51 -0300

    Update auxiliary.js
    Retirei a função conv.

--- nodejs/lib/auxiliary.js
+++ nodejs/lib/auxiliary.js
@@ -19,34 +19,5 @@ module.exports = {
     {
         let file = fs.readFileSync(path, 'base64');
         return file;
-    },
-    
-    conv: function(sinal1, sinal2)
-    {
-        let sn1_length = sinal1.length;
-        let sn2_length = sinal2.length;
-        let y = [];
-        let y_length = sinal1.length + sinal2.length;
-        let dl = Math.round((y_length-sn1_length)/2);
-
-        //formula da convolucao: u*v = y[n] = \sum_{m=0}^n u[m]v[n-m]
-
-        for(let i = dl; i<(dl+sn1_length); i++)
-        {
-            let k = i-dl;
-            //console.log(sn1_length-k);
-            y[k] = 0;
-            for(let j = 0; j<=i; j++)
-            {
-                if( (i-j) < 0 || (i-j)>=sn2_length)
-                    y[k] += 0;
-                else if(j<0 || j>=sn1_length)
-                    y[k] += 0;
-                else
-                    y[k] += sinal1[j]*sinal2[i-j];
-            }
-        }
-
-        return y;
     }
 };
