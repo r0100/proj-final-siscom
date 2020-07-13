1c0222893757060b367f378f95ba51247d34cc02
r0100 <lvinicius15@gmail.com>
2020-07-12 23:14:03 -0300

    Adicionei um começo de código para o back-end.

---
+++ interface_simples/server.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const http = require("http");
+//const util = require("util");
+const fs = require("fs");
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 8080;
+
+let html = fs.readFileSync("./index.html");
+let css = fs.readFileSync("./style.css");
+let source = fs.readFileSync("./source.js")
+
+///////////////////////////////////////////////////
+//Funções diversas:
+
+function parseBody(body_str)
+{
+    let div_str = body_str.split("&");
+    //console.log("argument body_str = " + body_str)
+    //console.log("div_str = " + div_str);
+    let out_obj = {};
+    //o código para quebrar as strings está estranho, mas funciona
+    for(let i = 0; i<div_str.length; i++)
+    {
+        let aux2 = "";
+        if(div_str[i]==="")
+            continue;
+
+        let aux = div_str[i].split("=");
+        aux[1] = aux[1].split("+");
+
+        for(let j = 0; j<aux[1].length; j++)
+        {
+            if(j>0)
+                aux2+=" ";
+            aux2+=aux[1][j];
+        }
+
+        //console.log(aux2);
+        out_obj[aux[0]]=aux2;
+        //console.log(out_obj);
+    }
+
+
+    return out_obj;
+}
+
+function extractPost(req)
+{
+    let promise = new Promise((resolve, reject) => {
+        let body_str = "";
+        req.on('data', (chunk) =>
+        {
+            //console.log(chunk.toString());
+            body_str += chunk.toString() + "&";
+        });
+        //sim, isto é uma gambiarra muito feia, eu não me orgulho desse código
+        setTimeout( () =>
+        {
+            if(body_str!=="")
+                resolve(body_str);
+            else
+                reject("ERRO!");
+        }, 0);
+    })
+
+    return promise;
+}
+///////////////////////////////////////////////////
+
+const server = http.createServer((req, res)=>
+{
+    if(req.method==="GET")
+    {
+        console.log("GET request received for " + req.url);
+        switch(req.url)
+        {
+          	case "/style.css":
+          	res.writeHead(200, {"Content-Type": "text/css"});
+          	res.write(css);
+          	break;
+          	case "/source.js":
+          	res.writeHead(200, {"Content-Type": "text/javascript"});
+          	res.write(source);
+          	break;
+          	default:
+          	res.writeHead(200, {"Content-Type": "text/html"});
+          	res.write(html);
+          	break;
+        }
+    }
+    else if(req.method==="POST")
+    {
+        console.log("POST request received");
+        extractPost(req)
+        .then(
+            function(body_str)
+            {
+                //console.log("body string = "+body_str);
+
+                let body = parseBody(body_str);
+                return body;
+            }
+        )
+        .then(body => console.log(body))
+        .catch(err => console.log("ERRO!"));
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/text');
+        res.setHeader('sucess', true);
+        res.end('Data received');
+    }
+        res.end();
+})
+server.listen(PORT, HOSTNAME, () =>
+{
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
--- interface_simples/source.js
+++ interface_simples/source.js
@@ -1,3 +1,7 @@
+/*TODO: simplificar funções de atualização*/
+
+'use strict'
+
 let onoff="off";
 let vol_value="50";
 let freq=(85+110)/2;
@@ -17,10 +21,71 @@ function printAll()
     console.log("flt="+flt);
 }
 
+function showInfoText(code, value)
+{
+    let text = "";
+    switch(code)
+    {
+	case "onoff":
+	text = "Status: " + ((value==="on")?"Ligado":"Desligado");
+	break;
+	case "vol_value":
+	text = "Volume: "+value+"%";
+	break;
+	case "freq":
+	text = "Frequência Central: "+value+"MHz";
+	break;
+	case "bnd_esq":
+	text = "Banda Esquerda: "+value+"MHz";
+	break;
+	case "bnd_dir":
+	text = "Banda Direita: "+value+"MHz";
+	break;
+	case "dmd":
+	text = "Método de Demodulação: " + ((value==="nenhum")?(value[0].toUpperCase()+value.substr(1)):value.toUpperCase());
+	break;
+	case "flt":
+	text = "Filtro anti-ruído: "+((value===true)?"Ligado":"Desligado");
+	break;
+	default:
+	text = "ERRO!";
+	break;
+    }
+
+    return text;
+}
+
+function initInfo()
+{
+    $("#frqshow").html(showInfoText("freq",freq));
+    $("#bndeqshow").html(showInfoText("bnd_esq", bnd_esq));
+    $("#bnddrshow").html(showInfoText("bnd_dir", bnd_dir));
+    $("#dmdshow").html(showInfoText("dmd", dmd));
+    $("#onoffshow").html(showInfoText("onoff", onoff));
+    $("#volshow").html(showInfoText("vol_value", vol_value));
+    $("#fltshow").html(showInfoText("flt", flt));
+}
+
 function sendServer(cond)
 {
-    if(cond===true)
-	$('#submit-btn').click();
+    if(cond===false)
+	return;
+
+    //$('#submit-btn').click();
+    $.ajax
+    ({
+    	method: "POST",
+    	url: "/receiver.js",
+    	data: "onoff="+onoff+"&vol_value="+vol_value+"&freq="+freq+"&bnd_esq="+bnd_esq+"&bnd_dir="+bnd_dir+"&dmd="+dmd+"&flt="+flt,
+    	contentType: "application/json; charset=utf-8",
+    	dataType: "json",
+    	async: true,
+    	cache: false,
+    	success: function(result){
+    	    console.log("Success");
+    	}
+    });
+
 }
 
 function updateStatus(status)
@@ -65,7 +130,7 @@ function updateFrq(id)
 function updateDemod(dmd_code)
 {
     let text = "";
-    
+
     switch(dmd_code)
     {
 	case "nenhum":
