59aa14e195e0d3f2657422b5f668f937d845bd64
mcvtec <mateuscvdev@gmail.com>
2020-07-17 22:24:55 -0300

    Makes stream demodulation work

--- nodejs/lib/stream/main-stream.js
+++ nodejs/lib/stream/main-stream.js
@@ -1,55 +1,55 @@
 'use strict'
 
 const fs = require('fs');
-const wavefile = require('wavefile');
 const {Transform} = require('stream');
 const Speaker = require('speaker');
 const wav  = require('wav');
 const readder = new wav.Reader();
 
 const { fmiqdemod, } = require('../demodulators'); 
-const { format } = require('path');
 
 let audio_filter = [0.0261, 0.1402, 0.3337, 0.3337, 0.1402, 0.0261];
 
 //código de teste de wav (o qual será usado) 
-let path = "../../../audio_test/teste_100900kHz.wav";
+let path = "../../../audio_test/SDRSharp_20200718_002948Z_94500000Hz_IQ.wav";
 
 const read_file_stream = fs.createReadStream(path);
-const speaker = new Speaker();
 
 
 const demodulate = new Transform({
     transform(chunk, encoding, cb) {
 
+        //Separate IQ components from  the stereo signal
         let iq = [[], []];
-
-        for(let i = 0; i<chunk.length-1; i+=4) {
-
-            iq[0].push(chunk[i]);
-
-            iq[1].push(chunk[i+1]);
-
+        for(let i = 0; i<chunk.length-1; i+=2) {
+            iq[0].push(chunk[i] - 127.5);
+            iq[1].push(chunk[i+1] - 127.5);
         }
 
-        for(let i = 0; i<iq[0].length; i++)
-        {
-            iq[0][i] -= 127.5;
-            iq[1][i] -= 127.5;
-        }
         //let y = am.amiqdemod(iq, audio_filter);
+        //Do the demodulation
         let y = fmiqdemod(iq, audio_filter);
+        
+        let out_audio_chunk = new Uint8Array(iq[0].length*2)
+        let y_mono = [];
+        for (let i =0; i<y.length-1; ++i) {
+            //Since the final audio data is mono, each channel must be equal
+            const rounded_val = Math.round(y[i]*40);
+            y_mono.push(rounded_val);
+            y_mono.push(rounded_val);
+        } 
 
-        let out = new Uint8Array(iq.length*2)
-      
-        this.push(out)
+        out_audio_chunk.set(y_mono);       
+        this.push(out_audio_chunk)
 
         cb();
     }
 })
 
 readder.on('format', (format) => {
-    readder.pipe(demodulate).pipe(speaker)
+    console.log(format)
+
+    readder.pipe(demodulate).pipe(new Speaker(format));
 })
 
 read_file_stream.pipe(readder);
